daf7f87a2f1cd416d7b61b7cfe476cb2dafea84f
nadia3190 <nadia.tlb.90@gmail.com>
2023-06-16 15:27:03 +0200

    adding Google Analytics Tracking tag

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -192,7 +192,7 @@
                     index = i;
                 }
             });
-            next = imagesCollection[index + 1] || imagesCollection[0];
+            next = imagesCollection[index + 1] || imagesCollection[0]; //bug fixed
             $(".lightboxImage").attr("src", $(next).attr("src"));
         },
         createLightBox(gallery, lightboxId, navigation) {
